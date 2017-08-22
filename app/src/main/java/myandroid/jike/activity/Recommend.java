7957fcc8a329244d4c3b22bc6563620ee1382fe6
yiqicute <302818696@qq.com>
2017-08-22 12:03:46 +0800

    master

---
+++ app/src/main/java/myandroid/jike/activity/Recommend.java
@@ -0,0 +1,71 @@
+package myandroid.jike.activity;
+
+import android.app.ListActivity;
+import android.support.v4.util.ArrayMap;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URI;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Objects;
+
+import myandroid.jike.R;
+public class Recommend extends ListActivity {
+    public SwipeRefreshLayout refreshLayout;
+    ListView recommendLv = null;
+    ArrayList<Map<String,Object>> mData= new ArrayList<Map<String,Object>>();
+
+        public static String getHtml(String path) throws Exception {
+            URL url = new URL(path);
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(2000);
+            connection.setDoOutput(true);
+            connection.setDoInput(true);
+            PrintWriter printWriter = new PrintWriter(connection.getOutputStream());
+            printWriter.write(post);
+            InputStream inputStream = connection.getInputStream();
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int len = 0;
+            while ((len = inputStream.read(buffer)) != -1){
+                outputStream.write(buffer, 0, len);
+            }
+            inputStream.close();
+            return outputStream.toString("utf-8");
+        }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recommend);
+
+        refreshLayout.setOnRefreshListener(this);
+        refreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                refreshLayout.setRefreshing(true);
+            }
+        });
+
+        recommendLv =getListView();
+
+
+        refreshData();
+        refreshLayout.setRefreshing(false);
+    }
+
+    public void refreshData() {
+        refreshData();
+    }
+
+}
---
+++ app/src/main/java/myandroid/jike/activity/SplashActivity.java
@@ -0,0 +1,33 @@
+package myandroid.jike.activity;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import myandroid.jike.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView iv = (ImageView)findViewById(R.id.splash_iv);
+        iv.setImageResource(R.drawable.splash);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2500);
+
+    }
+}
