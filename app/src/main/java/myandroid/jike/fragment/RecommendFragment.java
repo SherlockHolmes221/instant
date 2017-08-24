43721f7487a52c21717d9c25d2ae2f449c063684
yiqicute <302818696@qq.com>
2017-08-24 12:50:24 +0800

    推荐修改

--- app/src/main/java/myandroid/jike/fragment/RecommendFragment.java
+++ app/src/main/java/myandroid/jike/fragment/RecommendFragment.java
@@ -1,4 +1,4 @@
-package myandroid.jike.activity;
+package myandroid.jike.fragment;
 
 import android.app.ListActivity;
 import android.support.v4.util.ArrayMap;
@@ -22,27 +22,28 @@ public class Recommend extends ListActivity {
     public SwipeRefreshLayout refreshLayout;
     ListView recommendLv = null;
     ArrayList<Map<String,Object>> mData= new ArrayList<Map<String,Object>>();
+    public class item 
 
-        public static String getHtml(String path) throws Exception {
-            URL url = new URL(path);
-            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-            connection.setRequestMethod("POST");
-            connection.setConnectTimeout(5000);
-            connection.setReadTimeout(2000);
-            connection.setDoOutput(true);
-            connection.setDoInput(true);
-            PrintWriter printWriter = new PrintWriter(connection.getOutputStream());
-            printWriter.write(post);
-            InputStream inputStream = connection.getInputStream();
-            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-            byte[] buffer = new byte[1024];
-            int len = 0;
-            while ((len = inputStream.read(buffer)) != -1){
-                outputStream.write(buffer, 0, len);
-            }
-            inputStream.close();
-            return outputStream.toString("utf-8");
+    public static String getHtml(String path) throws Exception {
+        URL url = new URL(path);
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setConnectTimeout(5000);
+        connection.setReadTimeout(2000);
+        connection.setDoOutput(true);
+        connection.setDoInput(true);
+        PrintWriter printWriter = new PrintWriter(connection.getOutputStream());
+        printWriter.write(post);
+        InputStream inputStream = connection.getInputStream();
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        while ((len = inputStream.read(buffer)) != -1){
+            outputStream.write(buffer, 0, len);
         }
+        inputStream.close();
+        return outputStream.toString("utf-8");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +59,6 @@ public class Recommend extends ListActivity {
         });
 
         recommendLv =getListView();
-
 
         refreshData();
         refreshLayout.setRefreshing(false);
